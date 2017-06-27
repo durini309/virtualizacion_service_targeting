25033825479506a18fa151c492fd2620334add22
jdurini <jdurini@protelecom.com.gt>
2017-06-27 07:18:35 -0600

    Cambios a metodo

--- controllers/targeting_controller.js
+++ controllers/targeting_controller.js
@@ -17,32 +17,46 @@ module.exports = {
                 res.status(500).send({message: error.message});
             }
         });
+        var zipCode = parseInt(req.query['zip_code']);
+        var id = req.query['advertiser_campaigns'];
         
-        var query = "CALL get_allowed_campaigns(?)";
+        var query = 
+            'SELECT\n' + 
+            '   id\n' + 
+            'FROM\n' +
+            '   advertiser_campaigns\n' +
+            'WHERE' + 
+            '   (id IN (?) AND\n' +
+            "   (targeting like '%,?,%' OR targeting like '?,%' OR targeting like '%,?')) OR\n" +
+            "   (targeting is null OR targeting = '' OR targeting = 'ALL');";
+
         var params = [
-            req.params['advertiser_campaigns'], 
-            req.params['zip_code']
+            id, 
+            zipCode, 
+            zipCode, 
+            zipCode
         ];
 
+        
         connection.query(query, params, function(error, results, fields) {
             //Close connection
             connection.end();
-
+            console.log(query);
             //Internal server error, return 500
             if (error) {
                 console.log('ERROR - Problem with provided query: ' + error.message);
                 res.status(500).send({message: error.message});
             }
-            
+            console.log(results);
             //No records, return 404
-            if(results[0].length == 0){
+            if(results.length == 0){
                 console.log('ERROR - No records found');
                 res.status(400).send({message: 'ERROR - No records found'});
+            } else {
+                //Found records
+                console.log('OK - Successful query: "' + query + '" with results: ' + results);
+                res.status(200).send({'response' : results});
             }
-            
-            //Found records
-            console.log('OK - Successful query: "' + query + '" with results: ' + results[0]);
-            res.status(200).send(results[0]);
         }); 
     }
 };
